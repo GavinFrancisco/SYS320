2a30344706496cc9a4304482646708b20a2a4365
Gavin Francisco <gavin.francisco@mymail.champlain.edu>
2025-01-29 13:55:15 -0500

    working
    working

---
+++ windows/week3/FunctionsAndEventLogs.ps1
@@ -0,0 +1,28 @@
+﻿clear
+
+$loginouts = Get-Eventlog system -source Microsoft-Windows-Winlogon -After (Get-Date).Adddays(-14)
+
+$loginoutsTable = @()
+for ($i=0; $i -lt $loginouts.count; $i++){
+    #Creating Event value
+    $event = ""
+    if($loginouts[$i].instanceID -eq 7001){
+        $event = "logon"
+    }
+    if($loginouts[$i].instanceID -eq 7002){
+        $event = "logoff"
+    }
+    
+    #Creating User value
+    $SID = $loginouts[$i].ReplacementStrings[1]
+    $test = New-Object System.Security.Principal.SecurityIdentifier($SID)
+    $user = $test.Translate([System.Security.Principal.NTAccount])
+    
+    #Setting values in the table
+    $loginoutsTable += [pscustomobject]@{ "Time" = $loginouts[$i].TimeGenerated
+                                            "Id" = $loginouts[$i].EventID
+                                         "Event" = $event;
+                                          "User" = $user;
+                                        }
+}
+$loginoutsTable
