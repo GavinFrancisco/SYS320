4873a066ef6192067161e76ffbc5952e08822bbe
Gavin Francisco <gavin.francisco@mymail.champlain.edu>
2025-02-04 19:42:50 -0500

    done
    done
    
    Signed-off-by: Gavin Francisco <gavin.francisco@mymail.champlain.edu>

---
+++ windows/week3/Main.ps1
@@ -0,0 +1,10 @@
+﻿. (Join-Path $PSScriptRoot FunctionsAndEventLogs.ps1)
+
+clear
+
+# Get Login and Logoffs from the last 15 days
+getEventLogs 15
+# Get shut down from the last 25 days
+getOnOffLogs 6006 25
+# Get start up from the last 25 days
+getOnOffLogs 6005 25
