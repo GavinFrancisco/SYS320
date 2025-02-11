65d4de53b4c388efa5cfd50740ed4b965b6d0466
Gavin Francisco <gavin.francisco@mymail.champlain.edu>
2025-02-11 15:03:31 -0500

    Signed-off-by: Gavin Francisco <gavin.francisco@mymail.champlain.edu>

---
+++ windows/week4/ParsingApacheLogs.ps1
@@ -0,0 +1,21 @@
+﻿clear
+
+function apacheLogFilter(){
+    $logsUnFormatted = Get-Content C:\xampp\apache\logs\access.log
+    $tableRecords = @()
+    
+    for ($i = 0; $i -lt $logsUnFormatted.count; $i++){
+        $words =  $logsUnFormatted[$i].split(" ");
+
+        $tableRecords += [pscustomobject]@{ "IP" = $words[0]; 
+                                            "Time" = $words[3].Trim('['); 
+                                            "Method" = $words[5].Trim('"');
+                                            "Page" = $words[6];
+                                            "Protocol" = $words[7].Trim('"');
+                                            "Response" = $words[8];
+                                            "Referrer" = $words[9];
+                                            "Client" = $words[11..($words.count[-1])]}
+    }
+    
+    Return $tableRecords | Where-Object {$_.IP -ilike "10.*"}
+}
--- windows/week4/main.ps1
+++ windows/week4/main.ps1
@@ -1,5 +1,11 @@
 ﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+. (Join-Path $PSScriptRoot ParsingApacheLogs.ps1)
 
 clear
 
+#Testing WindowsApacheLogs
 apache-logs shrekfart 404 chrome
+
+#Testing ParsingApacheLogs
+$testing = apacheLogFilter
+$testing | Format-Table -AutoSize -Wrap
